215d5e32e44aafa47cc2da25be9bb0f759420594
Christian Kragegaard <mail@code.dk>
2021-06-28 13:22:34 +0200

    ejrtyerjgejb mere stuff

---
+++ Assets/goferihjgerj0i8j copy 3.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepScrene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        // Disable screen dimming
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
